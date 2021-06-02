0b17e8eb774925d73ec9c20faf9c8664e8ffd7ff
edcuth <38770919+edcuth@users.noreply.github.com>
2021-06-02 16:18:10 -0300

    second update project
    complete project

--- app.go
+++ app.go
@@ -5,12 +5,16 @@ import (
 	"fmt"
 	"log"
 
+	"encoding/json"
+	"net/http"
+	"strconv"
+
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
 
 type App struct {
-	router *mux.Router
+	Router *mux.Router
 	DB     *sql.DB
 }
 
@@ -18,14 +22,135 @@ func (this *App) Initialize(user, password, dbname string) {
 	connectionString := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", user, password, dbname)
 
 	var err error
-	a.DB, err = sql.Open("postgres", connectionString)
+	this.DB, err = sql.Open("postgres", connectionString)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	a.Router = mux.NewRouter()
+	this.Router = mux.NewRouter()
+	this.initializeRoutes()
 
 }
 
-func (this *App) Run(addr string) {}
+func (this *App) Run(addr string) {
+	log.Fatal(http.ListenAndServe(":8010", this.Router))
+}
+
+func (this *App) getProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid product ID")
+		return
+	}
+
+	p := product{ID: id}
+	if err := p.getProduct(this.DB); err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusNotFound, "Product not found")
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+	respondWithJSON(w, http.StatusOK, p)
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func (this *App) getProducts(w http.ResponseWriter, r *http.Request) {
+	count, _ := strconv.Atoi(r.FormValue("count"))
+	start, _ := strconv.Atoi(r.FormValue("start"))
+
+	if count > 10 || count < 1 {
+		count = 10
+	}
+	if start < 0 {
+		start = 0
+	}
+
+	products, err := getProducts(this.DB, start, count)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(w, http.StatusOK, products)
+}
+
+func (this *App) createProduct(w http.ResponseWriter, r *http.Request) {
+	var p product
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&p); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+
+	if err := p.createProduct(this.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(w, http.StatusCreated, p)
+}
+
+func (this *App) updateProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid product ID")
+		return
+	}
+
+	var p product
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&p); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+	p.ID = id
+
+	if err := p.updateProduct(this.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, p)
+}
+
+func (this *App) deleteProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid product ID")
+		return
+	}
+
+	p := product{ID: id}
+	if err := p.deleteProduct(this.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
+
+}
+
+func (this *App) initializeRoutes() {
+	this.Router.HandleFunc("/products", this.getProducts).Methods("GET")
+	this.Router.HandleFunc("/Product", this.createProduct).Methods("POST")
+	this.Router.HandleFunc("/product/{id:[0-9]+}", this.updateProduct).Methods("GET")
+	this.Router.HandleFunc("/product/{id:[0-9]+}", this.updateProduct).Methods("PUT")
+	this.Router.HandleFunc("/product/{id:[0-9]+}", this.deleteProduct).Methods("DELETE")
+}
--- model.go
+++ model.go
@@ -2,31 +2,55 @@ package main
 
 import (
 	"database/sql"
-	"errors"
 )
 
 type product struct {
 	ID    int     `json:"id"`
 	Name  string  `json:"name"`
-	Pirce float64 `json:"price"`
+	Price float64 `json:"price"`
 }
 
 func (this *product) getProduct(db *sql.DB) error {
-	return errors.New("Not implemented")
+	return db.QueryRow("SELECT name, price FROM products where id=$1", this.ID).Scan(&this.Name, &this.Price)
 }
 
 func (this *product) updateProduct(db *sql.DB) error {
-	return errors.New("Not implemented")
+	_, err := db.Exec("UPDATE products SET name=$1, price=$2, WHERE id=$3", this.Name, this.Price, this.ID)
+	return err
 }
 
 func (this *product) deleteProduct(db *sql.DB) error {
-	return errors.New("Not implemented")
+	_, err := db.Exec("DELETE FROM products WHERE id=$1", this.ID)
+
+	return err
 }
 
 func (this *product) createProduct(db *sql.DB) error {
-	return errors.New("Not implemented")
+	err := db.QueryRow("INSERT INTO products(name, price) VALUES ($1, $2) RETURNING id", this.Name, this.Price).Scan(&this.ID)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func getProdcut(dq *sql.DB, start, count int) ([]product, error) {
-	return nil, errors.New("Not implemented")
+func getProducts(db *sql.DB, start, count int) ([]product, error) {
+	rows, err := db.Query("SELECT id, name, price FROM products LIMIT $1 OFFSET $2", count, start)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	products := []product{}
+
+	for rows.Next() {
+		var p product
+		if err := rows.Scan(&p.ID, &p.Name, &p.Price); err != nil {
+			return nil, err
+		}
+		products = append(products, p)
+
+	}
+	return products, nil
 }
